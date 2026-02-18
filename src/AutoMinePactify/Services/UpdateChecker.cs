00e7176315749d478e5c8f6564d8190449081ac5
heyimbrioche <cbynth59@gmail.Com>
2026-02-18 18:14:23 +0100

    bump version to v1.5.0

--- src/AutoMinePactify/Services/UpdateChecker.cs
+++ src/AutoMinePactify/Services/UpdateChecker.cs
@@ -20,7 +20,7 @@ public static class UpdateChecker
     /// <summary>
     /// Version actuelle du programme. A incrementer a chaque release.
     /// </summary>
-    public const string CurrentVersion = "1.4.1";
+    public const string CurrentVersion = "1.5.0";
 
     /// <summary>
     /// Ton repo GitHub : "pseudo/nom-du-repo"
